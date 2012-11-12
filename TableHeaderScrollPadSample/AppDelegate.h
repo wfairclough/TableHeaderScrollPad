3d5049c319da5b4eee007b5420e48dbfbaa2dca4
Will Fairclough <wfairclough@gmail.com>
2012-11-12 15:22:20 -0400

    Initial Commit

---
+++ TableHeaderScrollPadSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableHeaderScrollPadSample
+//
+//  Created by Will Fairclough on 2012-11-12.
+//  Copyright (c) 2012 Will Fairclough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
